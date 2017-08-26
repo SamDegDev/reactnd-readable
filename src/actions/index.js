e786c1644fd54df9dfea7c02e427cf1981c93b6b
SamDegDev <samdegdev@outlook.com>
2017-08-26 22:47:03 +0100

    feat: added comments support

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,17 @@
 import * as ReadableAPI from '../utils/ReadableAPI';
 
+// Categories
 export const RECEIVE_ALL_CATEGORIES = 'RECEIVE_ALL_CATEGORIES';
 export const CHANGE_SELECTED_CATEGORY = 'CHANGE_SELECTED_CATEGORY';
+// Posts
 export const RECEIVE_ALL_POSTS = 'RECEIVE_ALL_POSTS';
 export const RECEIVE_POSTS_WITH_CATEGORY = 'RECEIVE_POSTS_WITH_CATEGORY';
 export const CHANGE_POSTS_SORTING = 'CHANGE_POSTS_SORTING';
 export const RECEIVE_POST_BY_ID = 'RECEIVE_POST_BY_ID';
+// Comments
+export const RECEIVE_ALL_COMMENTS_WITH_POST = 'RECEIVE_ALL_COMMENTS_WITH_POST';
+export const CHANGE_COMMENTS_SORTING = 'CHANGE_COMMENTS_SORTING';
+export const RECEIVE_COMMENT_BY_ID = 'RECEIVE_COMMENT_BY_ID';
 
 export const receiveAllCategories = categories => ({ type: RECEIVE_ALL_CATEGORIES, categories });
 export const fetchAllCategories = () => dispatch => {
@@ -24,17 +30,33 @@ export const fetchAllPosts = () => dispatch => {
 };
 
 export const receivePostsWithCategory = posts => ({ type: RECEIVE_POSTS_WITH_CATEGORY, posts });
-export const fetchPostsWithCategory = category => dispatch => {
+export const fetchPostsWithCategory = categoryId => dispatch => {
   ReadableAPI
-    .fetchPostsWithCategory(category)
+    .fetchPostsWithCategory(categoryId)
     .then(posts => dispatch(receivePostsWithCategory(posts)));
 }
 
 export const changePostsSorting = sorting => ({ type: CHANGE_POSTS_SORTING, sorting })
 
 export const receivePostById = post => ({ type: RECEIVE_POST_BY_ID, post });
-export const fetchPostById = id => dispatch => {
+export const fetchPostById = postId => dispatch => {
     ReadableAPI
-        .fetchPostById(id)
+        .fetchPostById(postId)
         .then(post => dispatch(receivePostById(post)));
 }
+
+export const receiveCommentsWithPost = comments => ({ type: RECEIVE_ALL_COMMENTS_WITH_POST, comments });
+export const fetchCommentsWithPost = postId => dispatch => {
+  ReadableAPI
+    .fetchCommentsWithPost(postId)
+    .then(comments => dispatch(receiveCommentsWithPost(comments)));
+}
+
+export const receiveCommentById = comment => ({ type: RECEIVE_COMMENT_BY_ID, comment });
+export const fetchCommentById = commentId => dispatch => {
+  ReadableAPI
+    .fetchCommentById(commentId)
+    .then(comment => dispatch(receiveCommentById(comment)));
+}
+
+export const changeCommentsSorting = sorting => ({ type: CHANGE_COMMENTS_SORTING, sorting })
