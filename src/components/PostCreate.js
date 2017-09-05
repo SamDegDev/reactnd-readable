67b608f30d227f42130383008fc555a419fc758a
SamDegDev <samdegdev@outlook.com>
2017-09-05 22:16:41 +0100

    feat: added form

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -4,34 +4,63 @@ import Shortid from 'shortid';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-//import { fetchCommentsWithPost, changeSelectedCategory } from '../actions';
-//import { urlize } from '../utils/helpers';
+import { createPost, fetchAllCategories } from '../actions';
+import serializeForm from 'form-serialize';
+import { capitalize } from '../utils/helpers';
 
 class PostCreate extends Component {
   componentDidMount() {
+    this.props.fetchAllCategories();
+  }
+
+  postData(e) {
+    e.preventDefault();
+    const data = serializeForm(e.target, { hash:true });
+    console.log(data);
+    //console.log(Shortid.generate());
+    //this.props.createPost(data);
   }
 
   render() {
-    //console.log(Shortid.generate());
+    const { categories } = this.props;
     return (
       <div>
         id, timestamp, title, body, owner, category
+        <form onSubmit={this.postData} className='create-post-form'>
+          <div className='create-post-details'>
+            <input type='text' name='title' placeholder='Title' />
+            <textarea name='body' />
+            <input type='text' name='owner' placeholder='Author' />
+            <select name='category'>
+              {categories.list.map(category =>
+                <option value={category.name} key={category.path}>{capitalize(category.name)}</option>
+              )}
+            </select>
+            <button>Add Post</button>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
-function mapStateToProps({ comments }) {
+function mapStateToProps({ categories }) {
   return {
+    categories,
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return {
+    createPost: postData => dispatch(createPost(postData)),
+    fetchAllCategories: data => dispatch(fetchAllCategories()),
   }
 };
 
 PostCreate.propTypes = {
+  createPost: PropTypes.func,
+  fetchAllCategories: PropTypes.func,
+  categories: PropTypes.object,
 };
 PostCreate.defaultProps = { extended: false };
 
